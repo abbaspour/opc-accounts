cd4fb3c7216e04f50dd55c0d58e0e818dd7201f3
Amin Abbaspour <a.abbaspour@gmail.com>
2020-12-29 14:40:47 +1100

    s3 policy attachment

--- modules/account/main.tf
+++ modules/account/main.tf
@@ -9,7 +9,7 @@ terraform {
   }
 }
 
-## Execution Role
+## -- Execution Role --
 data "aws_iam_policy_document" "ecs_task_execution_role" {
   version = "2012-10-17"
   statement {
@@ -34,6 +34,14 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+## -- Default Policy Object --
+resource "aws_s3_bucket_object" "default-bundle" {
+  bucket = var.policy_bucket
+  key    = "${var.account_no}/bundle.txt"
+  source = "${path.module}/bundle.txt"
+  etag = filemd5("${path.module}/bundle.txt")
+}
+
 ## Task Role
 data "aws_iam_policy_document" "ecs_task_role" {
   version = "2012-10-17"
@@ -47,6 +55,26 @@ data "aws_iam_policy_document" "ecs_task_role" {
       identifiers = ["ecs-tasks.amazonaws.com"]
     }
   }
+  /*
+  statement {
+    sid = ""
+    effect = "Allow"
+    actions = [
+      "s3:GetObject"
+    ]
+    resources = [
+      "arn:aws:s3:::${var.policy_bucket}/${var.account_no}/*"
+    ]
+  }
+  */
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecs-task-role-${var.account_no}"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_role.json
+}
+
+data "aws_iam_policy_document" "s3_data_bucket_policy" {
   statement {
     sid = ""
     effect = "Allow"
@@ -59,15 +87,12 @@ data "aws_iam_policy_document" "ecs_task_role" {
   }
 }
 
-resource "aws_iam_role" "ecs_task_role" {
-  name = "ecs-task-role-${var.account_no}"
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_role.json
+resource "aws_iam_policy" "s3_policy" {
+  name   = "s3-policy-${var.account_no}"
+  policy = data.aws_iam_policy_document.s3_data_bucket_policy.json
 }
 
-## Default Policy Object
-resource "aws_s3_bucket_object" "default-bundle" {
-  bucket = var.policy_bucket
-  key    = "${var.account_no}/bundle.txt"
-  source = "${path.module}/bundle.txt"
-  etag = filemd5("${path.module}/bundle.txt")
+resource "aws_iam_role_policy_attachment" "task_s3" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.s3_policy.arn
 }
