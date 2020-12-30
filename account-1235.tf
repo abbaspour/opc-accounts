6dc2d04f5cd40cfd004389b1fce8248c566d8291
Amin Abbaspour <a.abbaspour@gmail.com>
2020-12-30 15:55:19 +1100

    commit account 1235

---
+++ account-1235.tf
@@ -0,0 +1,11 @@
+module "account-1235" {
+  source = "./modules/account"
+  account_no = 1235
+  policy_bucket = var.policy_bucket
+  region = var.region
+  status_api_url = var.status_api_url
+  ecs_cluster_arn = var.ecs_cluster_arn
+  ecs_cluster_subnets = var.ecs_cluster_subnets
+  ecs_sg_id = var.ecs_sg_id
+  discovery_ns_id = var.discovery_ns_id
+}
