35c9a6ddd43de0f2ecd6b0c6b579d2df61dc69ff
Amin Abbaspour <a.abbaspour@gmail.com>
2021-01-17 10:27:07 +1100

    ignore etag not overwrite

--- modules/account/main.tf
+++ modules/account/main.tf
@@ -36,11 +36,12 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
 
 ## -- Default Policy Object --
 ## todo: publish to update-policy SQS rather than creating file manually
+## prevent overwrite
 resource "aws_s3_bucket_object" "default-bundle" {
   bucket = var.policy_bucket
   key    = "${var.account_no}/bundle.tar.gz"
   source = "${path.module}/bundle.tar.gz"
-  etag = filemd5("${path.module}/bundle.tar.gz")
+  #etag = filemd5("${path.module}/bundle.tar.gz")
 }
 
 ## Task Role
