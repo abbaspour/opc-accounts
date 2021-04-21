7aa118bd37de9425ca40d7cd7bd0940fda8a8b21
Amin Abbaspour <a.abbaspour@gmail.com>
2021-04-21 13:38:25 +1000

    commit account 100477427

---
+++ account-100477427.tf
@@ -0,0 +1,13 @@
+module "account-100477427" {
+  source = "./modules/account"
+  account_no = 100477427
+  policy_bucket = var.policy_bucket
+  region = var.region
+  status_api_url = var.status_api_url
+  ecs_cluster_arn = var.ecs_cluster_arn
+  ecs_cluster_subnets = var.ecs_cluster_subnets
+  ecs_sg_id = var.ecs_sg_id
+  discovery_ns_id = var.discovery_ns_id
+  vpc_id = var.vpc_id
+  listener_arn = var.listener_arn
+}
